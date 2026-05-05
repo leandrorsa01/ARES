f049ea2ad232d780482e6e8b3af7b5ae598a9e81
leandrorsa <leandroribeirosa@gmail.com>
2026-05-05 19:11:30 +0100

    #1

--- gainScheduling.m
+++ gainScheduling.m
@@ -8,8 +8,8 @@ max_quat_vetorial = sin(max_angulo / 2);
 Q = diag([
     1/max_quat_vetorial^2;   % 6: q2 (Pitch)
     1/max_quat_vetorial^2;   % 7: q3 (Yaw)
-    1/max_vel^2;             % 9: v
-    1/max_vel^2;             % 10: w
+    1000/max_vel^2;             % 9: v
+    1000/max_vel^2;             % 10: w
     1/max_taxas^2;           % 12: q (Pitch rate)
     1/max_taxas^2;           % 13: r (Yaw rate)
     ]);
@@ -17,8 +17,8 @@ Q = diag([
 max_gimbal = deg2rad(5);
 
 R = diag([
-    1/max_gimbal^2;        % 1: dy (Gimbal Pitch)
-    1/max_gimbal^2;        % 2: dz (Gimbal Yaw)
+    .01/max_gimbal^2;        % 1: dy (Gimbal Pitch)
+    .01/max_gimbal^2;        % 2: dz (Gimbal Yaw)
 ]);
 
 K_matrizes     = cell(1, length(pontos_nominais));
@@ -48,7 +48,7 @@ for i = 1:length(pontos_nominais)
             pad(nomes{i}, 8), max(real(E)));
 end
 
-evento = 1;
+evento = 8;
 
 fprintf('\n--- Análise Clássica de Amortecimento e Frequência (Ponto: %s) ---\n', nomes{evento});
 damp(Acl_matrizes{evento});
